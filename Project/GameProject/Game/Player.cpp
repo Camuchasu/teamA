810723537e7c601a606e72d6569d29922df5aa2b
hagumu1029 <24051906@d.anabuki-college.net>
2025-03-04 15:59:16 +0900

    Playerの作成０１

---
+++ Project/GameProject/Game/Player.cpp
@@ -0,0 +1,17 @@
+#include "Player.h"
+
+Player(const CVector2D)
+{
+};
+
+void Update()
+{
+}
+
+void Collision()
+{
+}
+
+void Draw()
+{
+}
--- Project/GameProject/Game/Player.h
+++ Project/GameProject/Game/Player.h
@@ -1,1 +1,8 @@
 #pragma once
+class Player
+{
+};
+Player(const CVector2D);
+void Update();
+void Collision();
+void Draw();
