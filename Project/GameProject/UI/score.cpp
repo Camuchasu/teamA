850e66534dd6764f63735bb36728ccebf1b6daa5
hagumu1029 <24051906@d.anabuki-college.net>
2025-03-18 15:26:41 +0900

    Loadの追加

--- Project/GameProject/UI/score.cpp
+++ Project/GameProject/UI/score.cpp
@@ -7,11 +7,11 @@ Score::Score() : Task((int)ETaskPrio ::UI, 0, eType_Score) {/*(int)‚ÍETaskPrio‚ð
 void Score::Render()
 {
 	int score = s_score;
-	for (int i = 0; i < 8; i++, score /= 10) {
+	for (int i = 0; i < 6; i++, score /= 10) {
 		int s = score % 10;
-		m_img.SetRect(16 * s, 16, 16 * s + 16, 32);
-		m_img.SetSize(16, 16);
-		m_img.SetPos(200 - 16 * i, 32);
+		m_img.SetRect(100 * s, 0,100 * s + 100, 128);
+		m_img.SetSize(150, 110);
+		m_img.SetPos(1700 - 102 * i, 10);
 		m_img.Draw();
 	}
 }
