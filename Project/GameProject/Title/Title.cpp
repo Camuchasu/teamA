64c22636798ec271f1d02c812832e6ff4ca439f8
hagumu1029 <24051906@d.anabuki-college.net>
2025-03-19 16:54:16 +0900

    Titleの変更

--- Project/GameProject/Title/Title.cpp
+++ Project/GameProject/Title/Title.cpp
@@ -5,6 +5,7 @@
 #include "Game/StarManager.h"
 #include "Game/Game.h"
 #include "Game/Star.h"
+#include "Game/Player.h"
 
 int Title::m_cnt(0);
 Title::Title() : Task((int)ETaskPrio::Title, 0, eType_Title)
@@ -14,19 +15,22 @@ Title::Title() : Task((int)ETaskPrio::Title, 0, eType_Title)
 	//画像複製
 	m_img = COPY_RESOURCE("Title", CImage);
 	m_img.SetSize(SCREEN_WIDTH, SCREEN_HEIGHT);
+	CVector3D pos(0, 0, 0);
+	new Player(0, pos, CVector3D(64, 180, 20), CVector3D(64, 0, 20));
 }
 
 void Title::Update()
 {
 	TitleCount++;
 	int StarType = rand() % 2;
-	if (TitleCount % (60*1) == 0) {
-		CVector3D pos(Utility::Rand(0.0f, 1920.0f), Utility::Rand(0.0f, 540.0f), Utility::Rand(0.0f, 0.0f));
+	if (TitleCount % (60*3) == 0) {
+		CVector3D pos(Utility::Rand(1500.0f, 1920.0f), Utility::Rand(0.0f, 0.0f), Utility::Rand(0.0f, -0.0f));
 		new Star (StarType,
-			pos, CVector3D(0, 0, 0), CVector3D(0, 0, 0));
+			pos + ObjectBase::m_scroll, CVector3D(64, 120, 20), CVector3D(64, 0, 20));
 	}
+	
 	//ボタン１でタイトル破棄
-	if (m_cnt++ > 1 && PUSH(CInput::eButton5)) {
+	if (m_cnt++ > 1 && PUSH(CInput::eButton10)) {
 		//すべてのオブジェクトを破棄
 		TaskManager::Instance()->DeleteAll();
 		SOUND("Title")->Stop();
